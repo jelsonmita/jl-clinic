b1e5f5a2fbb66adc9a3849dd76fde51de50ce613
Jelson Mita <jelsonmita0@gmail.com>
2022-04-25 00:22:58 +0100

    jl-clinic

--- assets/classes/users.php
+++ assets/classes/users.php
@@ -3,19 +3,19 @@
 class User
 {
     private $pdo;
-    public function conectar ($name, $host, $user, $password)
+    public function conectar ($name, $host, $user, $senha)
     {
         global $pdo;
         try {
             //code...
-            $pdo = new PDO("msql:dbname=".$name.";host=".$host,$user,$password);
+            $pdo = new PDO("msql:dbname=".$name.";host=".$host,$user,$senha);
         } catch (PDOException $e) {
             //throw $th;
             $msgError = $e->getMessage();
         }
     }
 
-    public function cadastrar($name, $sobrenome, $email, $data_nascimento, $password, $morada)
+    public function cadastrar($name, $sobrenome, $email, $data_nascimento, $senha, $morada)
     {
         global $pdo;
         //Verificar se o email já existe
@@ -30,13 +30,13 @@ class User
         else{
 
             //Senão, cadastrar
-            $sql = $pdo->prepare("INSERT INTO users (name, sobrenome, email, data_nascimento, password, morada) VALUES (:n, :s, :e, :p, :d, :m)");
+            $sql = $pdo->prepare("INSERT INTO users (name, sobrenome, email, data_nascimento, senha, morada) VALUES (:n, :s, :e, :S, :d, :m)");
             
             $sql->bindValue(":n", $name);
             $sql->bindValue(":s", $sobrenome);
             $sql->bindValue(":e", $email);
             $sql->bindValue(":d", $data_nascimento);
-            $sql->bindValue(":p", md5($password));
+            $sql->bindValue(":S", md5($senha));
             $sql->bindValue(":m", $morada);
             $spl->execute();
             return true();
@@ -47,9 +47,9 @@ class User
     {
         global $pdo;
         //Verificar de o email e a senha estão na database
-        $spl =  $pdo->prepare("SELECT id user FROM users WHERE email = :e AND password = :p");
+        $spl =  $pdo->prepare("SELECT id user FROM users WHERE email = :e AND senha = :p");
         $spl->blindValue(":e", $email);
-        $spl->blindValue(":p", md5($password));
+        $spl->blindValue(":p", md5($senha));
         $spl->execute();
 
         if($spl->rowCount() > 0);
